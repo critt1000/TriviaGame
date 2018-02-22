8f885df99a1236c3365ab21e4daef38ba69aabc8
Christian Rittmann <crittmann@hotmail.com>
2018-02-21 18:27:13 -0600

    2nd commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,6 +5,7 @@
 
 // ]
 
+
 var image = [src = "assets/images/amy.jpg", src = "assets/images/bender.jpg", src = "assets/images/brian.jpg", src = "assets/images/cartman.jpg", src = "assets/images/chris.jpg",
 src = "assets/images/goku.jpg", src = "assets/images/kenny.jpg", src = "assets/images/kyle.jpg", src = "assets/images/leela.jpg", src = "assets/images/lois.jpg",
 src = "assets/images/meg.jpg", src = "assets/images/morty.jpg", src = "assets/images/peter.jpg", src = "assets/images/phillip.jpg", src = "assets/images/piccolo.jpg",
@@ -21,19 +22,21 @@ var answer4 = ["alexa","folder", "brian", "archer", "ryan", "goten", "rogers", "
 "matt", "chi-chi", "zoidberg"];
 
 var questionCounter = 0;
-var userAnswers = [];
+var correctAnswers = [];
 var correct = 0;
 var wrong = 0;
 
 
 function nextQuestion() {
+    $("#a1", "#a2", "#a3", "#a4").empty();
     if (questionCounter < 21) {
         document.getElementById("image").src = image[questionCounter];
         // Add new values and text to input type="radio" & also collect users answers
-        $("#a1").after(answer1[questionCounter]);
-        $("#a2").after(answer2[questionCounter]);
-        $("#a3").after(answer3[questionCounter]);
-        $("#a4").after(answer4[questionCounter]);
+        $("#a1").attr("value", answer1[questionCounter]);
+        $("#label1").html(answer1[questionCounter]);
+        $("#label2").html(answer2[questionCounter]);
+        $("#label3").html(answer3[questionCounter]);
+        $("#label4").html(answer4[questionCounter]);
         questionCounter++;
     } else {
         // call win function
