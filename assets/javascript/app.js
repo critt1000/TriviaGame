5c7d5859d18b8740271358c75e202c9b8cd1c79c
Christian Rittmann <crittmann@hotmail.comgit config --global user.name Christian>
2018-06-22 22:48:10 -0500

    mostly working - issues with endgame / results display

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,47 +1,302 @@
-// var imageQuestions = [
-//     {image: "amy", src: "../images/amy.jpeg"},
-//     {question: "", answer1: "", answer2: "", answer3: "", answer4: ""},
-//     {image:}
 
-// ]
+//C:\Users\critt\bootcamp-projects\TriviaGame\assets\images\amy.jpg
+//C:\Users\critt\bootcamp-projects\TriviaGame\index.html
+//Question list
+var question = [{
+    image: src = "assets/images/amy.jpg",
+    answers: ["jessica", "mary", "amy", "alexa"],
+    correctAnswer: "amy"
+}, {
+    image: src = "assets/images/bender.jpg",
+    answers: ["bender", "fender", "grinder", "driller"],
+    correctAnswer: "bender"
+}, {
+    image: src = "assets/images/brian.jpg",
+    answers: ["brian", "greg", "jacob", "derek"],
+    correctAnswer: "brian"
+}, {
+    image: src = "assets/images/cartman.jpg",
+    answers: ["kyle", "kenny", "cartman", "stan"],
+    correctAnswer: "cartman"
+}, {
+    image: src = "assets/images/chris.jpg",
+    answers: ["chris", "christian", "christopher", "bob"],
+    correctAnswer: "chris"
+}, {
+    image: src = "assets/images/goku.jpg",
+    answers: ["goku", "frieza", "beerus", "cell"],
+    correctAnswer: "goku"
+}, {
+    image: src = "assets/images/kenny.jpg",
+    answers: ["kyle", "kenny", "cartman", "stan"],
+    correctAnswer: "kenny"
+}, {
+    image: src = "assets/images/kyle.jpg",
+    answers: ["kyle", "kenny", "cartman", "stan"],
+    correctAnswer: "kyle"
+}, {
+    image: src = "assets/images/leela.jpg",
+    answers: ["jessica", "leela", "amy", "alexa"],
+    correctAnswer: "leela"
+}, {
+    image: src = "assets/images/lois.jpg",
+    answers: ["jessica", "mary", "lois", "alexa"],
+    correctAnswer: "lois"
+}, {
+    image: src = "assets/images/meg.jpg",
+    answers: ["meg", "mary", "amy", "alexa"],
+    correctAnswer: "meg"
+}, {
+    image: src = "assets/images/morty.jpg",
+    answers: ["morty", "rick", "drew", "brock"],
+    correctAnswer: "morty"
+}, {
+    image: src = "assets/images/peter.jpg",
+    answers: ["tyler", "justin", "wiley", "peter"],
+    correctAnswer: "peter"
+}, {
+    image: src = "assets/images/phillip.jpg",
+    answers: ["drew", "gatlin", "philip", "ty"],
+    correctAnswer: "philip"
+}, {
+    image: src = "assets/images/piccolo.jpg",
+    answers: ["goku", "goten", "trunks", "piccolo"],
+    correctAnswer: "piccolo"
+}, {
+    image: src = "assets/images/professor.jpg",
+    answers: ["professor", "doctor", "physician", "surgeon"],
+    correctAnswer: "professor"
+}, {
+    image: src = "assets/images/rick.jpg",
+    answers: ["morty", "rick", "butch", "steve"],
+    correctAnswer: "rick"
+}, {
+    image: src = "assets/images/stan.jpg",
+    answers: ["kyle", "kenny", "cartman", "stan"],
+    correctAnswer: "stan"
+}, {
+    image: src = "assets/images/stewie.jpg",
+    answers: ["peter", "brian", "stewie", "chris"],
+    correctAnswer: "stewie"
+}, {
+    image: src = "assets/images/vegeta.jpg",
+    answers: ["goku", "vegeta", "trunks", "goten"],
+    correctAnswer: "vegeta"
+}, {
+    image: src = "assets/images/zoidburg.jpg",
+    answers: ["professor", "bender", "philip", "zoidburg"],
+    correctAnswer: "zoidburg"
+}]
 
-var image = [src = "assets/images/amy.jpg", src = "assets/images/bender.jpg", src = "assets/images/brian.jpg", src = "assets/images/cartman.jpg", src = "assets/images/chris.jpg",
-src = "assets/images/goku.jpg", src = "assets/images/kenny.jpg", src = "assets/images/kyle.jpg", src = "assets/images/leela.jpg", src = "assets/images/lois.jpg",
-src = "assets/images/meg.jpg", src = "assets/images/morty.jpg", src = "assets/images/peter.jpg", src = "assets/images/phillip.jpg", src = "assets/images/piccolo.jpg",
-src = "assets/images/professor.jpg", src = "assets/images/rick.jpg", src = "assets/images/stan.jpg", src = "assets/images/stewie.jpg", src = "assets/images/vegeta.jpg",
-src = "assets/images/zoidburg.jpg"]
+var timer;
+var counterStartValue = 20;
 
-var answer1 = ["jessica", "bender", "george", "kyle", "chris", "gohan", "derek", "steven", "leela", "shelli", "tiffani", "stanley", "peter", "trent", "dillan", "professor", "trey", "stan",
-"jay", "vegeta", "hubert"];
-var answer2 = ["mary", "fender", "bob", "cartman", "jacob", "goku", "andrew", "kyle", "layla", "pat", "meg", "rick", "toby", "drew", "piccolo", "engineer", "butch", "dan",
-"lance", "frieza", "hermes"];
-var answer3 = ["amy", "grinder", "beef", "doug", "gatlin", "trunks", "kenny", "andy", "loola", "lois", "meghan", "morty", "justin","donavan", "ty", "programmer", "rick", "doug",
-"stewie", "krillin", "fry"];
-var answer4 = ["alexa","folder", "brian", "archer", "ryan", "goten", "rogers", "sam", "heather", "terri", "christa", "ralph", "chad", "phillip", "brock", "doctor", "rocky", "lawerence",
-"matt", "chi-chi", "zoidberg"];
+var game = {
 
-var questionCounter = 0;
-var userAnswers = [];
-var correct = 0;
-var wrong = 0;
+    question: question,
+    currentQuestion: 0,
+    counter: 20,
+    correct: 0,
+    incorrect: 0,
 
+    clock: function() {
+        game.counter -= 1;
+        $("#timer-slot").text("Time left " + game.counter + "seconds");
+        if (game.counter === 0) {
+            game.timeUp();
+        }
+    },
 
-function nextQuestion() {
-    if (questionCounter < 21) {
-        document.getElementById("image").src = image[questionCounter];
-        // Add new values and text to input type="radio" & also collect users answers
-        $("#a1").after(answer1[questionCounter]);
-        $("#a2").after(answer2[questionCounter]);
-        $("#a3").after(answer3[questionCounter]);
-        $("#a4").after(answer4[questionCounter]);
-        questionCounter++;
-    } else {
-        // call win function
+    loadQuestion: function() {
+        timer = setInterval(game.clock, 1000);
+
+        $("#container-questions").empty();
+        $("#buttons").empty();
+        $("#results-log").empty(); //added
+        $("#container-questions").html("<img id='imageQuestion' src=" + question[this.currentQuestion].image + ">");
+        
+
+        for (var i = 0; i < question[this.currentQuestion].answers.length; i++) {
+            $("#buttons").append("<button class='answer-button' id='button' data-name='" + 
+            question[this.currentQuestion].answers[i] + "'>" + 
+            question[this.currentQuestion].answers[i] + "</button>");
+        }
+    },
+
+    nextQuestion: function() {
+        game.counter = counterStartValue;
+        $("#timer-slot").text("Time left: " + game.counter + "seconds");
+        game.currentQuestion += 1;
+        game.loadQuestion();
+    },
+
+    timeUp: function() {
+        clearInterval(timer);
+
+        $("#timer-slot").html("Time left: " + game.counter + "seconds");
+        if (game.currentQuestion === question.length - 1) {
+            setTimeout(game.results, 10 * 1000);
+        }
+        else {
+            setTimeout(game.nextQuestion, 2 * 1000);
+        }
+    },
+
+    results: function() {
+        clearInterval(timer);
+
+        $("#results").append("<h3>Correct: " + game.correct + "</h3>");
+        $("#results").append("<h3>Incorrect: " + game.incorrect + "</h3>");
+    },
+
+    clicked: function(e) {
+        clearInterval(timer);
+        if ($(e.target).attr("data-name") === question[this.currentQuestion].correctAnswer) {
+            this.answeredCorrectly();
+        }
+        else {
+            this.answeredIncorrectly();
+        }
+    },
+
+    answeredIncorrectly: function() {
+        game.incorrect += 1;
+        clearInterval(timer);
+
+        $("#results-log").html("<h3>That is wrong!</h3>");
+        $("#results-log").append("<h3> The correct answer is: " + question[game.currentQuestion].correctAnswer + "</h3>");
+    
+        if (game.currentQuestion === question.length - 1) {
+            setTimeout(game.results, 10 * 1000);
+        }
+        else {
+            setTimeout(game.nextQuestion, 2 * 1000);
+        }
+    },
+
+    answeredCorrectly: function() {
+        clearInterval(timer);
+
+        game.correct += 1;
+
+        $("#results-log").html("<h3>That is correct!</h3>");
+
+        if (game.currentQuestion === question.length - 1) {
+            setTimeout(game.results, 10 * 1000);
+        }
+        else {
+            setTimeout(game.nextQuestion, 2 * 1000);
+        }
+    },
+
+    reset: function() {
+        this.currentQuestion = 0;
+        this.counter = counterStartValue;
+        this.correct = 0;
+        this.incorrect = 0;
+        this.loadQuestion();
     }
-}
 
-function win(){
+};
 
-}
+$(document).on("click", "#reset", function() {
+    game.reset();
+});
 
+$(document).on("click", ".answer-button", function(e) {
+    game.clicked(e);
+});
+
+$(document).on("click", "#start", function() {
+    $("#container-timer").append("<h2> <span id='timer-slot'>20</span> Seconds</h2>");
+    game.loadQuestion();
+
+});
+
+// var timer;
+
+// var currentQuestion = 0;
+// var counter = 20;
+// var correct = 0;
+// var incorrect = 0;
+
+
+// function countdown() {
+//     counter -= 1;
+//     $("#timer-slot").text(counter);
+//     if (counter === 0) {
+//         console.log("Time is up")
+//         timeUp();
+//     };
+//     nextQuestion();
+// };
+
+// //Load the question
+//     //Need to change image src to current question
+//     //Need to load buttons with possible answers
+//     //Need to add one to currentQuestion
+// function loadQuestion () {
+//     $("#imageQuestion").attr("src", question[currentQuestion].src);
+//     currentQuestion += 1;
+//     fillButtons();
+// };
+
+
+// //Creates the buttons of possible answers
+//     //Need to go through question.answers
+// function fillButtons () {
+//     for (var i = 0; i < question.answers.length; i++) {
+//         switch(i) {
+//             case 0:
+//             $("#q1").empty();
+//             $("#q1").text(question[currentQuestion].answers[0]);
+//             break;
+
+//             case 1:
+//             $("#q2").empty();
+//             $("#q2").text(question[currentQuestion].answers[1]);
+//             break;
+
+//             case 2:
+//             $("#q3").empty();
+//             $("#q3").text(question[currentQuestion].answers[2]);
+//             break;
+
+//             case 3:
+//             $("#q4").empty();
+//             $("#q4").text(question[currentQuestion].answers[3]);
+//         };
+//     };
+// };
+
+// //
+// function nextQuestion () {
+
+// };
+
+// function timeUp () {
+//     clearInterval(timer);
+
+//     counter = 20;
+
+//     $("#timer-slot").empty().text(counter)
+// };
+
+// function answerCorrect () {
+    
+// };
+
+// function answerIncorrect () {
+
+// };
+
+// function reset () {
+
+// };
+
+// //============================================================
+// //Click events
+// $("#start").on("click", loadQuestion());
+
+// $("#next").on("click", nextQuestion());
 
